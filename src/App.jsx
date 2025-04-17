1bebfdf4af3debdf1d6f3b5591ca8e936f69bcc3
monom3r <ysnbakr5@gmail.com>
2025-04-18 02:42:07 +0300

    Implemented Navbar, Showcase section, and FeatureCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import FeatureCards from './components/FeatureCards';
+import LogoSection from './sections/LogoSection';
+import Navbar from './components/Navbar';
 import Hero from './sections/Hero';
+import AppShowcase from './sections/ShowcaseSection';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
+      <AppShowcase />
+      <LogoSection />
+      <FeatureCards />
     </>
   );
 };
